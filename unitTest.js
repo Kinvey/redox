4056dee5ec1d2d6fc6f4c195f0dd0a64457a678e
Michael Salinger <mjsalinger@kinvey.com>
2016-08-02 08:54:59 -0400

    updated unit tests

---
+++ unitTest.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2014, Kinvey, Inc. All rights reserved.
+ *
+ * This software contains valuable confidential and proprietary information of
+ * KINVEY, INC and is subject to applicable licensing agreements.
+ * Unauthorized reproduction, transmission or distribution of this file and its
+ * contents is a violation of applicable laws.
+ *
+ * Author: mjsalinger
+ */
+
+describe('connector', function() {
+  it('should connect to Redox', function(done) {
+    done();
+  });
+
+  it('should retrieve a patient record from Redox', function(done) {
+    done();
+  });
+
+  it('should return an error if a patient is not found', function(done) {
+    done();
+  });
+
+  it('should return an error if query is improperly formatted', function(done) {
+    done();
+  });
+
+});
