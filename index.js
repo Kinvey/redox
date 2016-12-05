201ce24f7fc99dfe441692d8ebd55e042608991f
Michael Salinger <mjsalinger@kinvey.com>
2016-12-05 07:08:02 -0500

    Updated to latest flex sdk

--- index.js
+++ index.js
@@ -1,76 +1,73 @@
 // redox integration
 
-var sdk = require('kinvey-backend-sdk');
-var redox = require('./redox.js');
+const sdk = require('kinvey-flex-sdk');
+const redox = require('./redox.js');
 
-console.log("starting redox poc");
-setInterval(function(){console.log("periodic ping")},60000); // just testing long running processes
+console.log('starting redox poc');
 
-var service = sdk.service(function(err, service) {
-  if (err)
-    console.log("error starting sdk service");
-  else
-    console.log("inside service");
+setInterval(() => {
+  console.log('periodic ping');
+}, 60000); // just testing long running processes
 
-  var dataLink = service.dataLink;   // gets the datalink object from the service
-  var notImplementedHandler = function(request, complete) {
-    complete("These methods are not implemented").notImplemented().done();
-  };
+sdk.service((err, flex) => {
+  if (err) {
+    console.log('error starting sdk service');
+  } else {
+    console.log('inside service');
+  }
+
+  const data = flex.data;   // gets the data object from the service
+  function notImplementedHandler(request, complete) {
+    complete('These methods are not implemented').notImplemented().done();
+  }
 
   // set the serviceObject
-  var patient = dataLink.serviceObject('Patient');
-  var summary = dataLink.serviceObject('ClinicalSummary');
+  const patient = data.serviceObject('Patient');
+  const summary = data.serviceObject('ClinicalSummary');
 
-  summary.onGetByQuery(function(request, complete) {
+  summary.onGetByQuery((request, complete) => {
     console.log('inside clinical summary query');
-    var demo = JSON.parse(request.query.query);
-    console.log("starting getByQuery for ",demo);
-    redox.getSummary(demo, function(result){
-      return complete(result).ok().next();
-    });
+    const demo = JSON.parse(request.query.query);
+    console.log('starting getByQuery for ', demo);
+    redox.getSummary(demo, (result) => complete(result).ok().next());
   });
 
   // wire up the event that we want to process
-  patient.onGetByQuery(function(request, complete) {
-    var q = JSON.parse(request.query.query);
-    console.log("starting getByQuery for ",q);
-    redox.searchPatient(q, function(result){
-      return complete(result).ok().next();
-    });
+  patient.onGetByQuery((request, complete) => {
+    const q = JSON.parse(request.query.query);
+    console.log('starting getByQuery for ', q);
+    redox.searchPatient(q, (result) => complete(result).ok().next());
   });
 
-  patient.onUpdate(function(request, complete) {
-    console.log("starting onUpdate for ",request.body);
-    redox.patientAdmin(request.body, function(result){
-      return complete(result).ok().next();
-    });
+  patient.onUpdate((request, complete) => {
+    console.log('starting onUpdate for ', request.body);
+    redox.patientAdmin(request.body, (result) => complete(result).ok().next());
   });
 
 
   // wire up the events that we are not implementing
-  patient.onGetById(function(request, complete) {
-    console.log("starting getById for ",request);
-    var entityId = request.entityId;
-    var entity = { _id:entityId, foo:"bar"};
+  patient.onGetById((request, complete) => {
+    console.log('starting getById for ', request);
+    const entityId = request.entityId;
+    const entity = { _id: entityId, foo: 'bar' };
 
     return complete(entity).ok().next();
   });
 
-  patient.onGetCount(function(request, complete) {
-    console.log("starting getCount for ",request);
-    var count = {count:1};
+  patient.onGetCount((request, complete) => {
+    console.log('starting getCount for ', request);
+    const count = { count: 1 };
     return complete(county).ok().next();
   });
 
-  var bizLogic = service.businessLogic;
+  const flexFunctions = flex.functions;
 
-  
-  var paasCollectionOnPreSave = function(request, complete) {
-    var entity = { "sam": 25 };
+  function paasCollectionOnPreSave(request, complete) {
+    const entity = { sam: 25 };
     return complete(entity).ok().next();
-  };
+  }
 
-  bizLogic.register('bizLogix', paasCollectionOnPreSave);
+  flexFunctions.register('bizLogix', paasCollectionOnPreSave);
 
   patient.onGetAll(notImplementedHandler);
   patient.onInsert(notImplementedHandler);
